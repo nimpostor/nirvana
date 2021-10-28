80976fa277b67a09ce1fa691a8ba34999e0a4aa9
Nimish Dhawan <nimpostor@pm.me>
2021-10-28 00:36:09 -0400

    make strategy configurable

--- coalesce.py
+++ coalesce.py
@@ -6,23 +6,27 @@ from strategy import *
 class CoalesceAPI:
     def __init__(self):
         self.apis = [MockAPI1(), MockAPI2(), MockAPI3()]
+        self.strategy = AvgStrategy
 
-    def get(self, member_id, strategy=Strategy.AVG):
+    def set_strategy(strategy: Strategy) -> int:
+        if strategy == Strategy.AVG:
+            self.strategy = AvgStrategy
+            return 0
+        elif strategy == Strategy.MIN:
+            self.strategy = MinStrategy
+            return 0
+        elif strategy == Strategy.MAX:
+            self.strategy = MaxStrategy
+            return 0
+        else:
+            return -1
+
+    def get(self, member_id: str) -> dict:
         responses = []
         for api in self.apis:
             responses.append(api.get(member_id))
 
-        if strategy == Strategy.MIN:
-            pass
-            #return get_min(responses)
-        elif strategy == Strategy.MAX:
-            pass
-            #return get_max(responses)
-        elif strategy == Strategy.COUNT:
-            pass
-            #return get_max_count(responses)
-        else:
-            return AvgStrategy.execute(responses)
+        return self.strategy.execute(responses)
 
 if __name__ == '__main__':
     x = CoalesceAPI()
